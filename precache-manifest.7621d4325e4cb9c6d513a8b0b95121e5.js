4e8d734cdb9dea7482904153b7bd64eeecd1294c
Prathik Malireddy <prathikmalireddy@gmail.com>
2020-08-02 17:10:24 -0500

    Updates

---
+++ precache-manifest.7621d4325e4cb9c6d513a8b0b95121e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74272290f5d49b3eb47575a3f835231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d91ed3738bc291792f87",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7bc8f3d2fd58ff54328",
+    "url": "/static/css/main.be3d1c19.chunk.css"
+  },
+  {
+    "revision": "d91ed3738bc291792f87",
+    "url": "/static/js/2.73c7bf1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.73c7bf1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7bc8f3d2fd58ff54328",
+    "url": "/static/js/main.1ee5ec84.chunk.js"
+  },
+  {
+    "revision": "66ebdc5490bf25d81731",
+    "url": "/static/js/runtime-main.435b1587.js"
+  },
+  {
+    "revision": "eb58f058b467cec86d2cd7f716675f60",
+    "url": "/static/media/IndexLabs_Colored.eb58f058.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
