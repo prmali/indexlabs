de208d7a1d6e76c186e92a3cf77b949c4e6b175b
Prathik Malireddy <prathikmalireddy@gmail.com>
2020-08-15 18:42:37 -0500

    Touching up website

--- src/components/home/breaker.jsx
+++ src/components/home/breaker.jsx
@@ -17,17 +17,51 @@ class Breaker extends React.Component {
                 <Row md="3" sm="1" xs="1">
                     <Col className="guide">
                         <h1>Guide</h1>
+                        <div className="container">
+                            <span className="section sec">
+                                <p className="date">01/01/2020</p>
+                                <div className="node"></div>
+                                <p className="description">Lorem Ipsum</p>
+                            </span>
+                            <span className="section prim">
+                                <p className="date">01/01/2020</p>
+                                <div className="node"></div>
+                                <p className="description">Lorem Ipsum</p>
+                            </span>
+                            <span className="section sec">
+                                <p className="date">01/01/2020</p>
+                                <div className="node"></div>
+                                <p className="description">Lorem Ipsum</p>
+                            </span>
+                        </div>
                     </Col>
 
                     <Col className="roadmap">
                         <h1>Roadmap</h1>
                         <div>
-                            <Button tag={Link} to="/Roadmap">Navigate</Button>
+                            <Button tag={Link} to="/roadmap">Navigate</Button>
                         </div>
                     </Col>
 
                     <Col className="community">
                         <h1>Community</h1>
+                        <div>
+                            <span className="media telegram">
+                                <p>Telegram</p>
+                            </span>
+                            <span className="media reddit">
+                                <p>Reddit</p>
+                            </span>
+                            <span className="media twitter">
+                                <p>Twitter</p>
+                            </span>
+                            <span className="media insta">
+                                <p>Insta</p>
+                            </span>
+                            <span className="media email">
+                                <p>Email</p>
+                            </span>
+                        </div>
                     </Col>
                 </Row>
             </div>
