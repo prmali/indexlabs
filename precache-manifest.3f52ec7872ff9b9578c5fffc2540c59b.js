acf69944e17488572825ae0b9e33fa20a373aca2
Prathik Malireddy <prathikmalireddy@gmail.com>
2020-09-01 20:35:27 -0500

    Updates

---
+++ precache-manifest.3f52ec7872ff9b9578c5fffc2540c59b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b1274fedcd83f90066397e3bc25d3c",
+    "url": "/indexlabs/index.html"
+  },
+  {
+    "revision": "31559dce30fcf73293e7",
+    "url": "/indexlabs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4cee80501045debff8f1",
+    "url": "/indexlabs/static/css/main.e1c75a30.chunk.css"
+  },
+  {
+    "revision": "31559dce30fcf73293e7",
+    "url": "/indexlabs/static/js/2.0ed82b33.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/indexlabs/static/js/2.0ed82b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cee80501045debff8f1",
+    "url": "/indexlabs/static/js/main.decc07d8.chunk.js"
+  },
+  {
+    "revision": "50a40a9e5f75e804498e",
+    "url": "/indexlabs/static/js/runtime-main.d33f854c.js"
+  },
+  {
+    "revision": "6a6afee9883d805ad542d74ca5a787ac",
+    "url": "/indexlabs/static/media/Globe-Icon.6a6afee9.png"
+  },
+  {
+    "revision": "e4d14d11300d00bc443ffc940c6ae622",
+    "url": "/indexlabs/static/media/Governance-Icon.e4d14d11.png"
+  },
+  {
+    "revision": "c095653297a858bfdf00c9253d7c1de0",
+    "url": "/indexlabs/static/media/Logo.c0956532.png"
+  },
+  {
+    "revision": "bb85daa98e7a74b3c25a11d31ab77072",
+    "url": "/indexlabs/static/media/Main.bb85daa9.png"
+  },
+  {
+    "revision": "15ec27326b65148a974f7826a7c5cbba",
+    "url": "/indexlabs/static/media/Market-Icon.15ec2732.png"
+  },
+  {
+    "revision": "411aec09e158ad579addd935532fe38d",
+    "url": "/indexlabs/static/media/email-icon.411aec09.png"
+  },
+  {
+    "revision": "74e0cae5f0b15ac9bdf9e33c0b203308",
+    "url": "/indexlabs/static/media/instagram-icon.74e0cae5.png"
+  },
+  {
+    "revision": "c5803eda175070b0a9d186e50fcf2cce",
+    "url": "/indexlabs/static/media/reddit-icon.c5803eda.png"
+  },
+  {
+    "revision": "66ad64016f8624556bcd8fc976a3906d",
+    "url": "/indexlabs/static/media/roadmap-image.66ad6401.png"
+  },
+  {
+    "revision": "fecb06fcafca884654ac96f43595b180",
+    "url": "/indexlabs/static/media/telegram-icon.fecb06fc.png"
+  },
+  {
+    "revision": "1684e981da340a762203cf85289b324b",
+    "url": "/indexlabs/static/media/token-allocation.1684e981.png"
+  },
+  {
+    "revision": "2c6d632da09cb9b7c001f7f06e3fcf4a",
+    "url": "/indexlabs/static/media/twitter-icon.2c6d632d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/indexlabs/precache-manifest.52af534b388e8340823b5474f453dbe0.js"
+  "/indexlabs/precache-manifest.3f52ec7872ff9b9578c5fffc2540c59b.js"
 );
 
 self.addEventListener('message', (event) => {
